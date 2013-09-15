c729b016e9d28f7bae1db207ccca24ac813a6fb3
Zathos <derrin.boerschel@gmail.com>
2013-09-15 00:46:33 -0500

    Simplest Scoring

--- 313Score/Form1.Designer.cs
+++ 313Score/Form1.Designer.cs
@@ -28,12 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Player1Score = new System.Windows.Forms.TextBox();
+            this.Player1 = new System.Windows.Forms.Label();
+            this.Player2 = new System.Windows.Forms.Label();
+            this.Player2Score = new System.Windows.Forms.TextBox();
+            this.ScoreButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Player1Score
+            // 
+            this.Player1Score.Location = new System.Drawing.Point(12, 54);
+            this.Player1Score.Name = "Player1Score";
+            this.Player1Score.Size = new System.Drawing.Size(45, 20);
+            this.Player1Score.TabIndex = 0;
+            // 
+            // Player1
+            // 
+            this.Player1.AutoSize = true;
+            this.Player1.Location = new System.Drawing.Point(12, 22);
+            this.Player1.Name = "Player1";
+            this.Player1.Size = new System.Drawing.Size(45, 13);
+            this.Player1.TabIndex = 1;
+            this.Player1.Text = "Player 1";
+            // 
+            // Player2
+            // 
+            this.Player2.AutoSize = true;
+            this.Player2.Location = new System.Drawing.Point(63, 22);
+            this.Player2.Name = "Player2";
+            this.Player2.Size = new System.Drawing.Size(45, 13);
+            this.Player2.TabIndex = 3;
+            this.Player2.Text = "Player 2";
+            // 
+            // Player2Score
+            // 
+            this.Player2Score.Location = new System.Drawing.Point(66, 54);
+            this.Player2Score.Name = "Player2Score";
+            this.Player2Score.Size = new System.Drawing.Size(42, 20);
+            this.Player2Score.TabIndex = 2;
+            // 
+            // ScoreButton
+            // 
+            this.ScoreButton.Location = new System.Drawing.Point(12, 80);
+            this.ScoreButton.Name = "ScoreButton";
+            this.ScoreButton.Size = new System.Drawing.Size(96, 31);
+            this.ScoreButton.TabIndex = 4;
+            this.ScoreButton.Text = "Score";
+            this.ScoreButton.UseVisualStyleBackColor = true;
+            this.ScoreButton.Click += new System.EventHandler(this.ScoreButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(124, 122);
+            this.Controls.Add(this.ScoreButton);
+            this.Controls.Add(this.Player2);
+            this.Controls.Add(this.Player2Score);
+            this.Controls.Add(this.Player1);
+            this.Controls.Add(this.Player1Score);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox Player1Score;
+        private System.Windows.Forms.Label Player1;
+        private System.Windows.Forms.Label Player2;
+        private System.Windows.Forms.TextBox Player2Score;
+        private System.Windows.Forms.Button ScoreButton;
+
     }
 }
 
--- 313Score/Form1.cs
+++ 313Score/Form1.cs
@@ -16,5 +16,24 @@ namespace _313Score
         {
             InitializeComponent();
         }
+
+        private void ScoreButton_Click(object sender, EventArgs e)
+        {
+            var player1Score = Int32.Parse(Player1Score.Text);
+            var player2Score = Int32.Parse(Player2Score.Text);
+
+            if (player1Score > player2Score)
+            {
+                MessageBox.Show("Player 1 Wins");
+            }
+            else if (player1Score < player2Score)
+            {
+                MessageBox.Show("Player 2 Wins");
+            }
+            else
+            {
+                MessageBox.Show("Tie");
+            }
+        }
     }
 }
